93d6c91bcd5e605995916c2f1da24a42d5f56ad9
Simon_Estrada <simon.lopeze@uqvirtual.edu.co>
2026-02-12 17:28:10 -0500

    Commit cree las 2 funciones ingresarEntero y mostrarmensaje

--- capicua/src/main/java/co/edu/uniquindio/Main.java
+++ capicua/src/main/java/co/edu/uniquindio/Main.java
@@ -1,11 +1,20 @@
 package co.edu.uniquindio;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     static void main(String[] args) {
         int numero = ingresarEntero("Ingresa tu numero: ");
         String mensaje = generarMensaje(numero);
         mostrarMensaje(mensaje);
+    }
+    public static int ingresarEntero (String mensaje){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(mensaje);
+        int entero = scanner.nextInt();
+        return entero;
+    }
+    //Daniela se encargara de realizar el metodo mensaje donde va a estar la logica
+    //para determinar si el numero es o no es capicua
+    public static void mostrarMensaje(String mensaje){
+        System.out.println(mensaje);
     }
 }
